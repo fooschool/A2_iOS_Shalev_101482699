471ed54dfd5e42151423b4dd6922b9c9b1d8f41f
da-shalev <44515019+da-shalev@users.noreply.github.com>
2026-04-07 18:14:00 -0400

    Initial commit

---
+++ A2_iOS_Shalev_101482699/A2_iOS_Shalev_101482699App.swift
@@ -0,0 +1,17 @@
+//
+//  A2_iOS_Shalev_101482699App.swift
+//  A2_iOS_Shalev_101482699
+//
+//  Created by Shalev Haimovitz on 2026-04-07.
+//
+
+import SwiftUI
+
+@main
+struct A2_iOS_Shalev_101482699App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
